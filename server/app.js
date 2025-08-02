e0a3818e3bedcc207dc3f5d238dc8208aef85bff
rjtkatoch007 <rjtkatoch007@gmail.com>
2025-08-02 16:53:56 +0530

    Database connected. Error handling middleware created.

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import {config} from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import { connection } from "./database/db.js"
+import { errorMiddleware } from "./middlewares/error.js"
 
 export const app = express()
 config({path: "./config.env"})
@@ -17,3 +19,7 @@ app.use(
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+connection()
+
+app.use(errorMiddleware)
